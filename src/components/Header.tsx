3d4ede1355b14830bfe8ced672e7610375f15dcd
Markus Schmitz <18517685+markusschmitz53@users.noreply.github.com>
2024-10-06 12:07:03 +0200

    feat: add base component structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="bg-white p-4 shadow-md">
+            Head
+        </header>
+    );
+}
---
+++ src/features/editor/components/Editor.tsx
@@ -0,0 +1,12 @@
+import Header from "../../../components/Header.tsx";
+
+export default function Editor() {
+    return (
+        <main>
+            <Header/>
+            <section>
+                works
+            </section>
+        </main>
+    );
+}
